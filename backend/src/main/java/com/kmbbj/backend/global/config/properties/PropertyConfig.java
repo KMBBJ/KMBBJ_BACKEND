54b63184fc8df687aec3e9729391a44c54b99b93
parkseokwon <parkswon1@naver.com>
2024-07-29 16:44:10 +0900

    🔧	구성 파일 추가
    도커yml을 작성했으니 모두 이걸 사용해서 동일한 환경의 dp를 사용하기로함
    
    propertyconfig을 사용해서 환경변수를 관리함

---
+++ backend/src/main/java/com/kmbbj/backend/global/config/properties/PropertyConfig.java
@@ -0,0 +1,12 @@
+package com.kmbbj.backend.global.config.properties;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:properties/.env")
+})
+public class PropertyConfig {
+}
