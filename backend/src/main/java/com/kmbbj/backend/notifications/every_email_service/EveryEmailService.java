73f373b803f9ead95a10243573638bf44822e938
hashi285 <m51187251@gmail.com>
2024-08-06 15:43:24 +0900

    🎨기능 추가 : 이메일 발송 클래스 DB 저장 기능 추가
    JavaMailSender를 주입받아 이메일 보내기 + 이메일 DB 저장 기능 추가

--- backend/src/main/java/com/kmbbj/backend/notifications/every_email_service/EveryEmailService.java
+++ backend/src/main/java/com/kmbbj/backend/notifications/every_email_service/EveryEmailService.java
@@ -1,25 +1,44 @@
 package com.kmbbj.backend.notifications.every_email_service;
 
 
+import com.kmbbj.backend.auth.entity.User;
+import com.kmbbj.backend.notifications.entity.EmailAlarm;
+import com.kmbbj.backend.notifications.repository.EmailAlarmRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+
 @Service
 @RequiredArgsConstructor
 public class EveryEmailService {
-    // JavaMailSender를 주입받아 이메일 발송 가능
 
+    // JavaMailSender를 주입받아 이메일 발송 가능
+    private final EmailAlarmRepository emailAlarmRepository;
     private final JavaMailSender mailSender;
 
     /**
-     * 간단한 이메일 메시지를 보내는 메소드
+     * 이메일 메시지를 보내는 메소드
+     * @param user 수신자 user 객체
      * @param to 수신자 이메일 주소
      * @param subject 이메일 제목
      * @param text 이메일 본문
+     * @param type String enum 값 (BYE, SELL, START, END, 공백 추가)
      */
-    public void sendSimpleMessage(String to, String subject, String text) {
+
+    public void sendSimpleMessage(User user, String to, String subject, String text, String type) {
+
+        // email_alarms 테이블 저장
+        EmailAlarm emailAlarm = new EmailAlarm();
+        emailAlarm.setUser(user);
+        emailAlarm.setSubject(subject);
+        emailAlarm.setMessage(text);
+        emailAlarm.setTradeOrder(EmailAlarm.TradeOrder.valueOf(type));
+        emailAlarm.setCreateDateAlarms(new Timestamp(System.currentTimeMillis()));
+        emailAlarmRepository.save(emailAlarm);
+
         // SimpleMailMessage 객체 생성 및 설정
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to); // 수신자 설정
