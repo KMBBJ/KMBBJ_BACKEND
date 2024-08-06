d4dc928bc64ce45f47274357fcef3dc245518664
parkseokwon <parkswon1@naver.com>
2024-08-06 13:43:11 +0900

    ✨	새 기능 : user sns 기능 추가

---
+++ backend/src/main/java/com/kmbbj/backend/auth/entity/UserSns.java
@@ -0,0 +1,25 @@
+package com.kmbbj.backend.auth.entity;
+
+import com.kmbbj.backend.global.entity.AuditingFields;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@ToString
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_sns")
+public class UserSns  extends AuditingFields {
+    @Id
+    @Column(name = "user_sns_id")
+    private Long userSnsId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "sns_id")
+    private Long snsId;
+}
