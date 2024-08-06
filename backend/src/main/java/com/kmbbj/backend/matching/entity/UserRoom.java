dad3825f45639586ac96537a5e5a96cc80f2bcf4
Jeayoung <jjy0843@gmail.com>
2024-08-06 23:13:42 +0900

    ✨ 새 기능 : 매칭 방 퇴장 , 입장

---
+++ backend/src/main/java/com/kmbbj/backend/matching/entity/UserRoom.java
@@ -0,0 +1,41 @@
+package com.kmbbj.backend.matching.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.kmbbj.backend.auth.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user_rooms")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_room_id", nullable = false)
+    private Long userRoomId;
+
+    @Column(name = "is_manager",nullable = false)
+    private Boolean isManager;
+
+    @Column(name = "is_played", nullable = false)
+    private Boolean isPlayed;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+}
---
+++ backend/src/main/java/com/kmbbj/backend/matching/service/UserRoomServiceImpl.java
@@ -0,0 +1,58 @@
+package com.kmbbj.backend.matching.service;
+
+import com.kmbbj.backend.auth.entity.User;
+import com.kmbbj.backend.auth.service.UserService;
+import com.kmbbj.backend.global.config.jwt.infrastructure.CustomUserDetails;
+import com.kmbbj.backend.matching.entity.Room;
+import com.kmbbj.backend.matching.entity.UserRoom;
+import com.kmbbj.backend.matching.repository.UserRoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserRoomServiceImpl implements UserRoomService{
+    private final UserRoomRepository userRoomRepository;
+    private final UserService userService;
+
+    @Override
+    public void save(UserRoom userRoom) {
+        userRoomRepository.save(userRoom);
+    }
+
+    /**
+     *
+     * @param user  접속 유저
+     * @param room  현재 방 위치
+     * @return userRoom
+     */
+    @Override
+    public UserRoom findByUserAndRoom(User user, Room room) {
+        return userRoomRepository.findByUserAndRoom(user, room);
+    }
+
+    /**
+     *
+     * @param roomId    현재 방 위치
+     * @param authentication    인증정보
+     */
+    @Override
+    public void deleteUserFromRoom(Long roomId,Authentication authentication) {
+
+        // 유저가 들어가 있는 방
+        UserRoom userRoom = findCurrentRoom(authentication);
+
+        // 방에서 플레이 여부 false
+        userRoom.setIsPlayed(false);
+        save(userRoom);
+    }
+
+    public UserRoom findCurrentRoom(Authentication authentication) {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        Long userId = userDetails.getUserId();
+        User user = userService.UserfindById(userId).orElseThrow();
+        UserRoom userRoom = userRoomRepository.findByUserAndIsPlayed(user, true);
+        return userRoom;
+    }
+}
