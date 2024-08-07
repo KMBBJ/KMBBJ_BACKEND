724d6030519f1eb0eb67452589377264849e0306
parkseokwon <parkswon1@naver.com>
2024-08-07 23:37:21 +0900

    ⚡️	성능 개선 : swagger 문서 커스텀 할수 있도록 설정 수정

--- backend/src/main/java/com/kmbbj/backend/global/config/swagger/SwaggerConfig.java
+++ backend/src/main/java/com/kmbbj/backend/global/config/swagger/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.kmbbj.backend.global.config.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +16,13 @@ public class SwaggerConfig {
                         .title("My API")
                         .version("1.0")
                         .description("This is a sample Spring Boot RESTful service using springdoc-openapi and OpenAPI 3."));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
     }
 }
