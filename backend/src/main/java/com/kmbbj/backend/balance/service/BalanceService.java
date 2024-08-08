90e45890afe39e292eeeffcd32af669a8b0c9298
parkseokwon <parkswon1@naver.com>
2024-08-09 00:10:30 +0900

    ✨	새 기능 : 사용자 id로 총 계좌 가져오는 서비스 로직 추가

---
+++ backend/src/main/java/com/kmbbj/backend/balance/service/BalanceService.java
@@ -0,0 +1,6 @@
+package com.kmbbj.backend.balance.service;
+
+import com.kmbbj.backend.balance.service.totalbalance.TotalBalanceService;
+
+public interface BalanceService extends TotalBalanceService {
+}
---
+++ backend/src/main/java/com/kmbbj/backend/balance/service/totalbalance/TotalBalanceService.java
@@ -0,0 +1,9 @@
+package com.kmbbj.backend.balance.service.totalbalance;
+
+import com.kmbbj.backend.balance.entity.TotalBalance;
+
+import java.util.Optional;
+
+public interface TotalBalanceService {
+    Optional<TotalBalance> totalBalanceFindByUserId(Long userId);
+}
