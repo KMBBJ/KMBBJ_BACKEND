4115ea6f02b9a63e3352d97bd55ced4f75a846ff
parkseokwon <parkswon1@naver.com>
2024-08-08 09:33:17 +0900

    🚑	긴급 수정 : user객체 시큐리티로부터 받아오는 법 수정

---
+++ backend/src/main/java/com/kmbbj/backend/global/config/security/FindUserBySecurity.java
@@ -0,0 +1,22 @@
+package com.kmbbj.backend.global.config.security;
+
+import com.kmbbj.backend.auth.entity.User;
+import com.kmbbj.backend.auth.service.UserService;
+import com.kmbbj.backend.global.config.exception.ApiException;
+import com.kmbbj.backend.global.config.exception.ExceptionEnum;
+import com.kmbbj.backend.global.config.jwt.infrastructure.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FindUserBySecurity {
+    private final UserService userService;
+
+    public User getCurrentUser() {
+        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userService.UserfindById(userDetails.getUserId()).orElseThrow(() -> new ApiException(ExceptionEnum.USER_NOT_FOUND));
+    }
+}
