ab6c0da5ee3af08a8127892bc86f297551fe7a38
kimkganghyun <163385332+kimkganghyun@users.noreply.github.com>
2024-08-13 10:47:19 +0900

    ✨ 새 기능 : 게임 데이터 관리를 위한 리포지토리 추가

---
+++ backend/src/main/java/com/kmbbj/backend/games/repository/RoundResultRepository.java
@@ -0,0 +1,19 @@
+package com.kmbbj.backend.games.repository;
+
+import com.kmbbj.backend.games.entity.RoundResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RoundResultRepository extends JpaRepository<RoundResult, Long> {
+
+    // 게임 ID로 라운드 결과 모두 조회
+    List<RoundResult> findByRoundGameGameIdOrderByRoundRoundIdAsc(Long gameId);
+
+    // 라운드 ID 통해 해당 라운드 결과 조회
+    Optional<RoundResult> findByRound_RoundId(Long roundId);
+
+}
