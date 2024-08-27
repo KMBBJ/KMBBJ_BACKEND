cd49397cf7312b88cb66f6baaec80ae8bd60d320
parkseokwon <parkswon1@naver.com>
2024-08-27 19:42:43 +0900

    🎨	코드의 구조 : requset에 생성자 등록

--- backend/src/main/java/com/kmbbj/backend/feature/exchange/controller/request/TransactionsRequest.java
+++ backend/src/main/java/com/kmbbj/backend/feature/exchange/controller/request/TransactionsRequest.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Schema(name = "사용자가 거래 기록을 요청할때 보내는 아이디")
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class TransactionsRequest {
     @Schema(name = "사용자 아이디")
     private Long userId;
