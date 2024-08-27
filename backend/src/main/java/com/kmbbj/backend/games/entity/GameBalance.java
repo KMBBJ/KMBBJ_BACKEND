cabbaa5a57e25233a2da9fa6815c89bf2a62fac7
kimkganghyun <163385332+kimkganghyun@users.noreply.github.com>
2024-08-27 18:05:08 +0900

    💩 똥싼 코드: 기존 Room 아닌데  Game 변경
    주석좀 추가 했습니다!

--- backend/src/main/java/com/kmbbj/backend/games/entity/GameBalance.java
+++ backend/src/main/java/com/kmbbj/backend/games/entity/GameBalance.java
@@ -1,7 +1,6 @@
 package com.kmbbj.backend.games.entity;
 
 import com.kmbbj.backend.auth.entity.User;
-import com.kmbbj.backend.matching.entity.Room;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,15 +21,16 @@ public class GameBalance {
 
     // 게임 잔액 (기본 키)
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "game_balances_id")
     private Long gameBalancesId;
 
     /**
-     * 게임 계좌 속한 룸 (게임 세션)
-     * 다대일 관계 , 하나의 게임 여러 게임 계좌 존재
+     * 하나의 게임에 여러 게임 계좌 존재
+     * 게임 A 있다면 사용자 1 2 3 가 A 참여하면 게임 A 계좌 3개 생성
      */
     @ManyToOne
-    @JoinColumn(name = "room_id", nullable = false)
-    private Room room;
+    @JoinColumn(name = "game_id", nullable = false)
+    private Game game;
 
     /**
      *  게임 계좌의 소유자인 사용자
@@ -39,4 +39,10 @@ public class GameBalance {
     @OneToOne
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
+
+    /**
+     * 게임에서 사용자가 보유한 시드머니(자금)
+     */
+    @Column(name = "seed", nullable = false)
+    private Integer seed;
 }
