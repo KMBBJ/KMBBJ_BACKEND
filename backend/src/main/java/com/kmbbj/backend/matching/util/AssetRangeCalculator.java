bb4d06e333655b6aeb2361278bab9743f13251bb
jeayoung <jjy0843@gmail.com>
2024-08-30 14:31:15 +0900

    ♻️ 코드 리팩토링 : 상수값 환경변수로 관리

--- backend/src/main/java/com/kmbbj/backend/matching/util/AssetRangeCalculator.java
+++ backend/src/main/java/com/kmbbj/backend/matching/util/AssetRangeCalculator.java
@@ -1,11 +1,16 @@
 package com.kmbbj.backend.matching.util;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class AssetRangeCalculator {
 
     // 5분을 시간으로 변환 (5분 / 60분)
-    private final double t = 0.0833;
+    @Value("${ASSET_RANGE_T}")
+    private double T;
+
     // 목표 자산 범위
-    private final double x = 10;
+    @Value(("${ASSET_RANGE_X"))
+    private double X;
 
 
 
@@ -13,10 +18,10 @@ public class AssetRangeCalculator {
     public double calculateAssetRange(double t) {
 
         // b 값을 찾기 위해 목표 자산 범위 식을 재정리
-        double desiredExp = (x - 5) / 15;
+        double desiredExp = (X - 5) / 15;
 
         // e^(-b * t) = 1 - desiredExp 식을 b에 대해 풀기
-        double b = -Math.log(1 - desiredExp) / t;
-        return 5 + 15 * (1 - Math.exp(-b * t));
+        double b = -Math.log(1 - desiredExp) / (T * t);
+        return 5 + 15 * (1 - Math.exp(-b * (T * t)));
     }
 }
