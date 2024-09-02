cbecdde1da9caedfe64982dcff8b250ab9258889
parkseokwon <parkswon1@naver.com>
2024-09-02 09:57:26 +0900

    👷	CI 빌드 시스템 추가 : db 변경

--- backend/src/main/resources/sql/table/drop.sql
+++ backend/src/main/resources/sql/table/drop.sql
@@ -1,6 +1,9 @@
-drop table users CASCADE;
-drop table user_sns;
-
-drop table coins CASCADE;
-drop table coin24h_details;
-drop table kline;
+DO $$
+    DECLARE
+        r RECORD;
+    BEGIN
+        -- 모든 테이블을 루프를 통해 삭제
+        FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
+                EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+            END LOOP;
+    END $$;
