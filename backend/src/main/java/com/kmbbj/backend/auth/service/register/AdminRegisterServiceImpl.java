02d0804729eebdd3f9107521efc9a8a66af09a5a
hashi285 <m51187251@gmail.com>
2024-09-04 00:02:21 +0900

    🗃 유저 권한 수정 + 관리자 회원가입 기능 추가
    USER/ADMIN -> ROLE_USER/ROLE_ADMIN + 관리자 회원가입 기능 추가

---
+++ backend/src/main/java/com/kmbbj/backend/auth/service/register/AdminRegisterServiceImpl.java
@@ -0,0 +1,69 @@
+package com.kmbbj.backend.auth.service.register;
+
+
+import com.kmbbj.backend.auth.controller.request.UserJoinRequest;
+import com.kmbbj.backend.auth.entity.Authority;
+import com.kmbbj.backend.auth.entity.User;
+import com.kmbbj.backend.auth.repository.UserRepository;
+import com.kmbbj.backend.auth.util.RandomNickname;
+import com.kmbbj.backend.balance.entity.TotalBalance;
+import com.kmbbj.backend.balance.service.BalanceService;
+import com.kmbbj.backend.global.config.exception.ApiException;
+import com.kmbbj.backend.global.config.exception.ExceptionEnum;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminRegisterServiceImpl implements AdminRegisterService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RandomNickname randomNickname;
+    private final BalanceService balanceService;
+
+    /**
+     * 어드민 등록 처리
+     *
+     * @param userJoinRequest 회원가입 요청 데이터 (어드민 전용)
+     */
+    @Transactional
+    @Override
+    public void registerAdmin(UserJoinRequest userJoinRequest) {
+        // 이메일 중복 확인
+        if (userRepository.findByEmail(userJoinRequest.getEmail()).isPresent()) {
+            throw new ApiException(ExceptionEnum.EXIST_EMAIL);
+        }
+
+        // 비밀번호 인코딩
+        String encodedPassword = passwordEncoder.encode(userJoinRequest.getPassword());
+
+        // User 엔티티로 변환 및 저장
+        User admin = User.builder()
+                .email(userJoinRequest.getEmail())
+                .nickname(getNickname())
+                .password(encodedPassword)
+                .authority(Authority.ROLE_ADMIN)  // 어드민 권한 설정
+                .isDeleted(false)
+                .build();
+
+        TotalBalance totalBalance = TotalBalance.builder()
+                .asset(30000000L)
+                .user(admin)
+                .build();
+
+        userRepository.save(admin);
+        balanceService.makeTotalBalance(totalBalance);
+    }
+
+    /**
+     * 중복되지 않은 랜덤 닉네임 생성
+     *
+     * @return unique random nickname
+     */
+    public String getNickname() {
+        String nickname = randomNickname.generate();
+        return nickname;
+    }
+}
